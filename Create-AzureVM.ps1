38dd54cafe4862b46e1065609ba4c0cce6f5f24d
Vaggelis Kappas <vakappas@microsoft.com>
2018-11-26 21:35:03 +0200

    Initial Check-in

---
+++ Create-AzureVM.ps1
@@ -0,0 +1,13 @@
+#login to your azure account
+Login-AzureRmAccount
+Get-AzureRmSubscription
+
+Select-AzureRmSubscription -Subscription "vakappas - Internal Consumption"
+
+#Deploy VM to Azure using Template
+    New-AzureRmResourceGroup -Name "WSLabRGInsider" -Location "West Europe"
+    $TemplateUri="https://raw.githubusercontent.com/Microsoft/WSLab/master/Scenarios/Running%20WSLab%20in%20Azure/WSLabServerInsider.json"
+    New-AzureRmResourceGroupDeployment -Name WSLabInsider -ResourceGroupName WSLabRGInsider -TemplateUri $TemplateUri -Verbose
+
+#connect to VM using RDP
+    mstsc /v:((Get-AzureRmPublicIpAddress -ResourceGroupName WSLabRGInsider).IpAddress)
